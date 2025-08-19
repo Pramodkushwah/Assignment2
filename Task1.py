fd229297cf87e445522faeec14510e04b8f494ec
Pramodkushwah <devidsking@gmail.com>
2025-08-20 00:51:46 +0530

    Done

---
+++ Task1.py
@@ -0,0 +1,5 @@
+A=int(input("Enter a number: "))
+if A % 2 == 0:
+    print(f"{A} is an even number")
+else:
+    print(f"{A} is an odd number")
---
+++ Task2.py
@@ -0,0 +1,6 @@
+a=0
+for num in range(1,51):
+    a += num
+print("The sum of numbers from 1 to 50 is:", a)
+
+
